81fb14e66c8167d5bd4548f50dee8bfc9d5266c1
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-09-06 10:33:30 +0700

    Update type-declarations.go

--- type-declarations.go
+++ type-declarations.go
@@ -1,3 +1,8 @@
+/**
+Type Declarations adalah kemampuan membuat ulang tipe data baru dari tipe data yang sudah ada
+Type Declarations biasanya digunakan untuk membuat alias terhadap tipe data yang sudah ada,
+dengan tujuan agar lebih mudah dimengerti
+*/
 package main
 
 import "fmt"
